a76ed9215359fcea6951e45e60d35d4d6d03ac91
VladNCode <vldsmrfow@gmail.com>
2021-12-11 10:30:54 +0200

    built features section and styled it

--- cw.js
+++ cw.js
@@ -4,3 +4,12 @@ console.log(hello);
 console.log('Suh dude');
 
 console.log(10 + 30);
+
+const hide = document.querySelector(".hideme");
+const btn = document.querySelector(".btn");
+hide.style.opacity = "1";
+
+btn.addEventListener("click", () => {
+  if (hide.style.opacity === "1") hide.style.opacity = "0";
+  else hide.style.opacity = "1";
+});
